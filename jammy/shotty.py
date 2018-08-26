e39648063ee4579897c86f4a559ea3a758d3d7a0
Jamal Mohamed <JamalM@corp.aspiredigital.net>
2018-08-26 11:34:37 +0400

    The update of code which enables us to start andstop instances and creating snapshot of the instances.

--- jammy/shotty.py
+++ jammy/shotty.py
@@ -13,25 +13,103 @@ def filter_instances(project):
     else:
         instances = ec2.instances.all()
     return instances
-
 @click.group()
+def cli():
+    """Jammy manages Snapshot"""
+
+@cli.group('snapshots')
+def snapshots():
+    """Command for Snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None, help="Only snapshots for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 Snapshots"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(', '.join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.progress,
+                    s.state,
+                    s.start_time.strftime("%c")
+                )))
+    return
+
+
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None, help="Only volumes for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 Volumes"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(', '.join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted",
+            )))
+    return
+
+@cli.group('instances')
 def instances():
     """Commands for Instances"""
+
+@instances.command('snapshot', help="Create snapshots for all volumes")
+@click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
+def create_snapshots(project):
+    "Create Snapshot for EC2 Instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+
+        i.stop()
+        i.wait_until_stopped()
+
+        for v in i.volumes.all():
+            print("  Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created for snapshotalyzer-30000")
+
+        print("Starting {0}...".format(i.id))
+
+        i.start()
+        i.wait_until_running()
+
+    print("Job is done!")
+
+    return
+
 @instances.command('list')
 @click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
 def list_instances(project):
     "List EC2 Instances"
+
     instances = filter_instances(project)
 
     for i in instances:
         tags = { t['Key']: t['Value'] for t in i.tags or [] }
         print(', '.join((
-        i.id,
-        i.instance_type,
-        i.placement['AvailabilityZone'],
-        i.state['Name'],
-        i.public_dns_name,
-        tags.get('Project', '<no project>')
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name,
+            tags.get('Project', '<no project>')
         )))
     return
 
@@ -63,4 +141,4 @@ def stop_instances(project):
 
 
 if __name__ == '__main__':
-    instances()
+    cli()
